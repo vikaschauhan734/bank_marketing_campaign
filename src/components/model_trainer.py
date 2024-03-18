82c159ecd14b3180d60e0e827edceaee33cff0d2
Vikas Chauhan <chauhanvikas734@gmail.com>
2024-03-18 22:02:07 +0530

    hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -13,7 +13,7 @@ from sklearn.metrics import classification_report, confusion_matrix, precision_s
 
 from src.exception import CustomException
 from src.logger import logging
-from src.utils import save_object, evaluate_models
+from src.utils import save_object, balance_data, evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
@@ -26,6 +26,8 @@ class ModelTrainer:
     def initiate_model_trainer(self, train_array, test_array):
         try:
             logging.info("Split training and test input data")
+            train_array = balance_data(train_array)
+            test_array = balance_data(test_array)
             X_train, y_train, X_test, y_test = (
                 train_array[:,:-1],
                 train_array[:,-1],
@@ -45,7 +47,61 @@ class ModelTrainer:
                 "GaussianNB": GaussianNB()
             }
 
-            model_report:dict = evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test, models=models)
+            params={
+                "Logistic Regression": {
+                    'penalty':['l1','l2','elasticnet']
+                },
+                "SVC":{
+                    'kernel':['linear','poly','rbf','sigmoid','precomputed'],
+                    'gamma':['scale','auto']
+                },
+                "K-Neighbors Classifier":{
+                    'n_neighbors':[4,5,6,7,10],
+                    'weights':['uniform','distance'],
+                    'algorithm':['auto','ball_tree','kd_tree','brute']
+                },
+                "Decision Tree Classifier":{
+                    'criterion':['gini','entropy','log_loss'],
+                    'splitter':['best','random'],
+                    'max_features':['sqrt','log2']
+                },
+                "Random Forest Classifier":{
+                    'n_estimators': [10,25,31,50],
+                    'max_depth': [10,13,15],
+                    'min_samples_split': [3,4,5],
+                    'min_samples_leaf': [5,6,7]
+                },
+                "Ada Boost Classifier":{
+                    'n_estimators':[10,15,20,30,50],
+                    'learning_rate':[0.01,0.1,1,2,5,10]
+                },
+                "XGBClassifier":{
+                    'booster':['gbtree','gblinear'],
+                    'eta':[0.1,0.3,0.5],
+                    'gamma':[0,1,10,50,100],
+                    'max_depth':[0,1,5,10]
+                },
+                "Gradient Boosting Classifier":{
+                    'loss':['log_loss','exponential'],
+                    'learning_rate':[0.1,0.25,0.5,1],
+                    'n_estimators':[10,20,50,75,100],
+                    'subsample':[0.1,0.25,0.5,0.75,1],
+                    'criterion':['friedman_mse','squared_error'],
+                    'min_samples_split':[2,5,10],
+                    'min_samples_leaf':[1,2,10]
+                },
+                "Bagging Classifier":{
+                    'n_estimators':[1,5,10,15,20],
+                    'max_samples':[0.1,1,10],
+                    'max_features':[0.1,1,10]
+                },
+                "GaussianNB":{
+
+                }
+
+            }
+
+            model_report:dict = evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test, models=models,params=params)
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
 
@@ -55,8 +111,8 @@ class ModelTrainer:
             ]
             best_model = models[best_model_name]
 
-            # if best_model_score<0.6:
-            #     raise CustomException("No best model found")
+            if best_model_score<0.6:
+                raise CustomException("No best model found")
             logging.info("Best model found on the both training and testing dataset")
             
             save_object(
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,13 @@
 import os
 import sys
 from src.exception import CustomException
+import json
 import dill
 import numpy as np
 import pandas as pd
-from sklearn.metrics import fbeta_score
+from imblearn.over_sampling import SMOTE
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import fbeta_score,accuracy_score, precision_score,recall_score,f1_score
 
 
 def save_object(file_path, obj):
@@ -19,20 +22,44 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e,sys)
     
+def balance_data(array):
+    smote = SMOTE(sampling_strategy='minority')
+    X, y = smote.fit_resample(array[:,:-1],array[:,-1])
+    return np.concatenate((X,y.reshape(-1,1)), axis=1)
+    
 
-def evaluate_models(X_train,y_train,X_test,y_test, models):
+def evaluate_models(X_train,y_train,X_test,y_test, models, params):
     try:
         report = {}
+        score = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            param=params[(list(model.keys()))[i]]
+            gs = GridSearchCV(model,param,cv=5)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
-            # beta = 2, because emphaisising 
+            train_acc = accuracy_score(y_train,y_train_pred)
+            train_pre = precision_score(y_train, y_train_pred)
+            train_re = recall_score(y_train, y_train_pred)
+            train_f1 = f1_score(y_train,y_train_pred)
+            test_acc = accuracy_score(y_test,y_test_pred)
+            test_pre = precision_score(y_test, y_test_pred)
+            test_re = recall_score(y_test, y_test_pred)
+            test_f1 = f1_score(y_test,y_test_pred)
+            # beta = 2, because emphaisizing recall
             train_model_score = fbeta_score(y_train, y_train_pred,beta=2)
             test_model_score = fbeta_score(y_test, y_test_pred,beta=2)
             report[list(models.keys())[i]] = test_model_score
+            score[list(models.keys())[i]] = {"train":{"acccuracy":train_acc,"precision":train_pre,"recall":train_re,"f1":train_f1,"fbeta":train_model_score},
+                                             "test":{"acccuracy":test_acc,"precision":test_pre,"recall":test_re,"f1":test_f1,"fbeta":test_model_score}}
+        score_file = os.path.join("artifacts","score.json")
+        with open(score_file,"w") as f:
+            json.dump(score, f, indent=4)
         return report
     except Exception as e:
         raise CustomException(e, sys)
